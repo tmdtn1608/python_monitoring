8f6cb8033ef6a69d621f772dea32bab0214ef313
Seungsu Baek <tmdtn1608@gmail.com>
2024-10-21 18:13:42 +0900

    도중작업중

--- hw02_bss.py
+++ hw02_bss.py
@@ -1,5 +1,6 @@
 # 파일명 number_영어이니셜.py
-# 숫자 1개를 입력받아서 숫자가 크면 "숫자가 커요"
+# 1부터 100까지 중에 숫자를 입력하세요
+# 입력받아서 숫자가 크면 "숫자가 커요"
 # 숫자가 작으면 "숫자가 작아요"
 # 숫자가 같으면 "숫자가 같아요"
 # 비교할 숫자는 30, 변수명 num1
@@ -8,11 +9,15 @@ num1 : int = 30
 
 num2 = input("숫자를 입력하세요")
 if(not num2.isdigit()) :
-    print("숫자가 아니에요")
+    print("1부터 100까지 중에 숫자를 입력하세요")
 else :
-    if(int(num2) > num1) :
-        print("숫자가 커요")
-    elif(int(num2) == num1) :
-        print("숫자가 같아요")
-    else : 
-        print("숫자가 작아요")
+    num2_cast : int = int(num2)
+    if(num2_cast < 1 or num2_cast > 100) : 
+        print("숫자는 1부터 100 사이")
+    else :
+        if(num2_cast > num1) :
+            print("숫자가 커요")
+        elif(num2_cast == num1) :
+            print("숫자가 같아요")
+        else : 
+            print("숫자가 작아요")

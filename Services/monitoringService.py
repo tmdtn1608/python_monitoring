983281c7a5db073261fb72b64410df786a783c7d
BaekSeungSu <tmdtn1608@gmail.com>
2024-10-26 13:41:39 +0900

    통신구축

--- Services/monitoringService.py
+++ Services/monitoringService.py
@@ -1,3 +1,5 @@
+import json
+from typing import List
 import psutil
 import platform
 
@@ -14,19 +16,24 @@ class monitoringService():
     def getProcess(self): 
         if self.os_name == 'Windows': # Windows에 대한 함수 호출
             print("Running Windows specific function.")
-            self.getWindowsProcess()
+            return self.getWindowsProcess()
         elif self.os_name == 'Linux': # Linux에 대한 함수 호출
             print("Running Linux specific function.")
         elif self.os_name == 'Darwin':  # macOS의 경우
             print("Running macOS specific function.")
-            self.getMacProcess()
+            return self.getMacProcess()
         else:
             print("Unknown Operating System.")
             return
         
-    def getMacProcess(self) :
+    def getMacProcess(self) -> json:
+        process_list = []
         for proc in psutil.process_iter(['pid', 'name', 'status', 'cpu_percent', 'memory_percent']):
-            print(proc.info)
+            process_info = proc.info
+            process_list.append(process_info)
+
+        json_output = json.dumps(process_list, indent=4)
+        return json_output
 
     def getWindowsProcess(self) :
         # pid가 windows에서 지원하는지 확인해볼 것.
--- main.py
+++ main.py
@@ -2,113 +2,79 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Color, Rectangle
 from kivy.core.window import Window
 from kivy.core.text import LabelBase
 from kivy.clock import Clock
 import asyncio
 import websockets
 import requests
+import json
 import threading
 
+from Services.monitoringService import monitoringService
+
 class MyApp(App):
+    
     def build(self):
         self.title = "monitor"
-        Window.size = (300, 120)  # 윈도우 크기
-        Window.left = 400 # 오픈되는 윈도우 위치, left
-        Window.top = 100 # 오픈되는 윈도우 위치, top
-        Window.resizable = False # 크기조정
-        Window.borderless = False # borderless
+        Window.size = (300, 120)
+        Window.left = 400
+        Window.top = 100
+        Window.resizable = False
 
-        # 사용할 폰트 설정 (TTF 파일 경로)
-        #/Users/seungsubaek/Downloads/D2Coding-Ver1.3.2-20180524/D2Coding
         LabelBase.register(name='MyFont', fn_regular='./Resources/D2Coding-Ver1.3.2-20180524.ttf')
-        
-        # 종료 플래그 설정
-        self.stop_event = threading.Event()
 
-        # GridLayout 설정
-        grid = GridLayout(
-            cols=2, # 열 수
-            rows=3, # 행 수
-            padding=15, # 여백
-            spacing=15, # 위젯 간 간격
-            # size_hint=(0.5, 0.5), # 크기 비율, 확인 필요.....
-            # pos_hint={'center_x': 0.4, 'center_y': 0.75}, # 위치 비율
-            row_force_default=True, # 열 크기 강제
-            row_default_height=60, # 열 크기 강제시 강제할 정도
-            col_force_default = True, # 기본 너비 강제 적용
-            col_default_width = 150 # 기본 열 너비 설정
-        )
+        grid = GridLayout(cols=2, rows=3, padding=15, spacing=15)
 
-        # 라벨 및 버튼 추가
-        grid.add_widget(
-            Label(text='서버상태',font_name="MyFont", size_hint_x=None,width=150)
-        )
-        grid.add_widget(
-            Label(text='정상', font_name="MyFont", size_hint_x=None,width=150)
-        )
-        grid.add_widget(
-            Label(text='라이센스',font_name="MyFont", size_hint_x=None,width=150)
-        )
-        grid.add_widget(
-            Label(text='라이센스 유효', font_name="MyFont", size_hint_x=None,width=200)
-        )
-        grid.add_widget(
-            Label(text='서버전송',font_name="MyFont", size_hint_x=None,width=150)
-        )
-        send_btn = Button(text='Button 2',
-                          font_name="MyFont", 
-                          size_hint_x=None, 
-                          width=150)
+        grid.add_widget(Label(text='서버상태', font_name="MyFont"))
+        grid.add_widget(Label(text='정상', font_name="MyFont"))
+        grid.add_widget(Label(text='라이센스', font_name="MyFont"))
+        grid.add_widget(Label(text='라이센스 유효', font_name="MyFont"))
+        grid.add_widget(Label(text='서버전송', font_name="MyFont"))
+
+        send_btn = Button(text='Button 2', font_name="MyFont")
         send_btn.bind(on_press=self.send_request)
         grid.add_widget(send_btn)
-        # Clock.schedule_once(lambda dt: asyncio.run(self.connect()), 0)
-        Clock.schedule_once(lambda dt: self.start_websocket_thread(), 0)
+
+        # Initialize the monitoring service
+        self.monitoring_service = monitoringService()
+
+        # Start the WebSocket thread
+        threading.Thread(target=self.start_websocket_thread, daemon=True).start()
 
         return grid
-    
-    def send_request(self, instance) :
-        print("send request")
-        # TODO : http request로 전송.
+
+    def send_request(self, instance):
         response = requests.get("http://localhost:3000/")
         if response.status_code == 200:
-            print(response.text)  # JSON 데이터를 출력
+            print(response.text)
         else:
             print(f"Request failed with status code {response.status_code}")
 
     def start_websocket_thread(self):
-        # WebSocket 연결을 스레드에서 실행
-        thread = threading.Thread(target=asyncio.run, args=(self.connect(),))
-        thread.start()
+        # Run the asyncio event loop in this thread
+        asyncio.run(self.connect())
 
     async def connect(self):
-        uri = "ws://localhost:3000/ws" 
+        uri = "ws://localhost:3000/ws"
         try:
             async with websockets.connect(uri) as websocket:
                 print("Connected to WebSocket!")
-                
-                # 연결 유지 및 메시지 수신
-                while not self.stop_event.is_set():
-                    await websocket.send("Ping")
-                    response = await websocket.recv()
-                    print("Server response:", response)
-                    await asyncio.sleep(5)
+                while True:
+                    await self.send_process_info(websocket)
+                    await asyncio.sleep(5)  # Wait before sending the next message
         except websockets.ConnectionClosed:
             print("Connection closed")
 
+    async def send_process_info(self, websocket):
+        process_info = self.monitoring_service.getProcess()
+        print("============================\n",process_info) # why return None?
+        json_data = json.dumps(process_info)
+        await websocket.send(json_data)  # Send the JSON data
+        print("Sent process info:", json_data)
+
     def on_stop(self):
-        # 앱이 종료될 때 종료 플래그 설정
-        print("Stopping the app and WebSocket connection...")
-        self.stop_event.set()
-
-
-'''
-라이센스 정보 획득
-'''
-def get_license_info() -> bool :
-    return True
+        print("Stopping the app...")
 
 if __name__ == '__main__':
     MyApp().run()

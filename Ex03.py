7db66e3426328f03be7513743b285422eeddff5b
ssbaek <ssbaek@finaldata.com>
2024-10-28 12:57:10 +0900

    숙제 함수화

--- Ex03.py
+++ Ex03.py
@@ -15,19 +15,22 @@ def is_num(param: str) -> bool :
 """
 반복 input
 """
-def input_num(cnt: int) -> bool | int :
+def input_num(cnt: int) -> bool | str :
     num = input(f"숫자 {cnt}를 입력하세요") # input -> str
     if(not is_num(num)) : return False
     else : return num
 
 """
-While & break로 진행관리
+main
 """
-while (True) :
-    num1 = input_num(1)
-    if(num1 == False) : break
+def main() :
+    while (True) :
+        num1 = input_num(1)
+        if(num1 == False) : break
 
-    num2 = input_num(2)
-    if(num2 == False) : break
+        num2 = input_num(2)
+        if(num2 == False) : break
 
-    print(f"{int(num1) + int(num2)}")
+        print(f"{int(num1) + int(num2)}")
+
+main()

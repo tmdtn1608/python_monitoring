870c9bd3512e473bc7dab2e90cabdeadfcfeed9d
BaekSeungSu <tmdtn1608@gmail.com>
2024-11-21 19:53:18 +0900

    클라이언트 빌드 진행

---
+++ Ex04-1.py
@@ -0,0 +1,60 @@
+'''
+리스트 [1, [2, 3], [4, [5, 6]]]를 [1, 2, 3, 4, 5, 6]으로 평탄화하는 함수를 작성하세요.
+-> recursive
+SOLVED
+'''
+original : list = [1, [2, 3], [4, [5, 6]]]
+def iterable_flatten(param : list) :
+    flatten : list = []
+    for i in param :
+        if(type(i) == list) : 
+            print('list : ', i)
+            flatten.extend(iterable_flatten(i))
+        elif(type(i) == int) : 
+            print('int : ',i)
+            flatten.append(i)
+    return flatten
+   
+print(iterable_flatten(original))
+
+'''
+리스트 [1, 2, 3, 4, 5]의 모든 부분 리스트를 생성하는 함수를 작성하세요.
+'''
+def sub_list() : 
+    original : list = [1, 2, 3, 4, 5]
+    return
+
+'''
+두 리스트의 요소를 번갈아가며 새 리스트로 만드는 함수를 작성하세요. 
+(예: [1,2,3], [a,b,c] → [1,a,2,b,3,c])
+SOLVED
+'''
+def merge_list() :
+    original_1 : list = [1,2,3]
+    original_2 : list = ['a','b','c']
+    result : list = []
+    length : int = len(original_1) if len(original_1) > len(original_2) else len(original_2)
+    for i in range(length) :
+        try:
+            result.append(original_1[i])
+            result.append(original_2[i])
+        except IndexError:
+            pass
+    return result
+print(merge_list())
+
+'''
+리스트에서 n번째로 큰 요소를 찾는 함수를 작성하세요.
+SOLVED
+'''
+def high_element(idx : int) : 
+    original : list = [1,3,4,6,7,9]
+    if(idx-1 >= len(original)) : return
+    sort_list = sorted(original, reverse=True)
+    return sort_list[idx-1]
+
+print(high_element(2))
+
+'''
+리스트의 연속된 부분 리스트 중 합이 가장 큰 부분을 찾는 함수를 작성하세요. -> ????
+'''
--- README.md
+++ README.md
@@ -7,3 +7,18 @@
 3. ```.venv/bin/python -m pip install -r requirements.txt```로 가상환경에 의존성 추가
 4. 가상환경에서 실행
 5. 작업 완료후 ```deactivate```로 가상환경 비활성
+
+### pyinstaller
+클라이언트 디렉토리로 이동후 진행
+- 일반환경
+```
+pyinstaller --onefile --name ProcessMonitor main.py 
+```
+- 가상환경
+```
+/가상환경/경로/bin/python -m PyInstaller --name ProcessMonitor --onefile main.py
+```
+- 실제 
+```
+~/ProcessMonitor/.venv/bin/pyinstaller --name ProcessMonitor --add-data "Resources/*.ttf:Resources" --add-data ".env:.env"  main.py
+```
